efb1977e676dbc1e9da43e679a9356871bb3df5c
Faisal Ayub <fayub@payactiv.com.pk>
2023-11-05 07:24:32 -0800

    apply modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FamilyChartComponent } from './pages/family-chart/family-chart.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { UpdateFamilyMemberComponent } from './components/update-family-member/update-family-member.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     FamilyChartComponent,
+    UpdateFamilyMemberComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/update-family-member/update-family-member.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-family-member',
+  templateUrl: './update-family-member.component.html',
+  styleUrls: ['./update-family-member.component.scss']
+})
+export class UpdateFamilyMemberComponent {
+
+}
